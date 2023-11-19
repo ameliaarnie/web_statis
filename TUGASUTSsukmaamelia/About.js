9019cd13366a0e04aed46234f5d39fd918cf8d82
ameliaarnie <144694239+ameliaarnie@users.noreply.github.com>
2023-11-19 20:56:34 +0700

    Add files via upload

---
+++ TUGASUTSsukmaamelia/About.js
@@ -0,0 +1,23 @@
+// sidebar
+const menu = document.querySelector('.navbar #menu');
+const nav = document.querySelector('.navbar .navbar-nav');
+
+menu.addEventListener('click', function() {
+    nav.classList.toggle('active')
+});
+
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+  }
